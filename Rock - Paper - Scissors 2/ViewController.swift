d8d37af0c1eb5a54871ccf1cacd1adb60e9ed974
Ezgi İrem Kaltalıoğlu <ezgikaltalioglu25@gmail.com>
2023-07-09 19:08:16 +0300

    Initial Commit

---
+++ Rock - Paper - Scissors 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rock - Paper - Scissors 2
+//
+//  Created by Ezgi Kaltalıoğlu on 9.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rock - Paper - Scissors 2UITests/Rock___Paper___Scissors_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Rock___Paper___Scissors_2UITestsLaunchTests.swift
+//  Rock - Paper - Scissors 2UITests
+//
+//  Created by Ezgi Kaltalıoğlu on 9.07.2023.
+//
+
+import XCTest
+
+final class Rock___Paper___Scissors_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
